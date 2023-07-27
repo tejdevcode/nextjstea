549976765f5261aa6e86b749451b2097af351018
tejrdev <tejashkumar.r@uplers.in>
2023-07-27 07:57:07 +0530

    page generate

--- app/(pages)/about/page.jsx
+++ app/(pages)/about/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Aboutpage = () => {
   return (
-    <div>Aboutpage</div>
+    <div>Aboutpage</div> 
   )
 }
 
---
+++ app/components/page/Pageheader.jsx
@@ -0,0 +1,23 @@
+// import WOW from 'react-wow';
+// import Link from 'next/link';
+
+// const Pageheader = ({data}) => {
+//    return (
+//       // <WOW animation='fadeIn' delay="0.1s">
+//          <div class="container-fluid page-header py-5 mb-5 ">
+//             <div class="container text-center py-5">
+//                <h1 class="display-2 text-dark mb-4 animated slideInDown">{data.pagename}</h1>
+//                <nav aria-label="breadcrumb animated slideInDown">
+//                   <ol class="breadcrumb justify-content-center mb-0">
+//                      <li class="breadcrumb-item"><Link href="#">Home</Link></li>
+//                      <li class="breadcrumb-item"><Link href="#">Pages</Link></li>
+//                      <li class="breadcrumb-item text-dark" aria-current="page">{data.pagebrad}</li>
+//                   </ol>
+//                </nav>
+//             </div>
+//          </div>
+//       // </WOW>
+//    )
+// }
+
+// export default Pageheader
